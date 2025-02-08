cb019723dedd2ed49f1188334d3a2989db9d5b00
purva sharma <sharmapurva5598@gmail.com>
2025-02-08 16:21:24 +0530

    typo

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import ToDoList from '../ToDoList/ToDoList';
 import Calendar from '../Calendar/Calendar';
-import PieChart from '../PieChart/PieChart';
+import PieChart from '../PieChart/Piechart';
 import EventTracker from '../EventTracker/EventTracker';
 import './Dashboard.css';
 
